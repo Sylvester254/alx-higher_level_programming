518fe480006cb20831a389b5d367fa0b961ea333
Sylvester254 <slynganga59@gmail.com>
2022-06-28 18:11:40 +0300

    alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for alphabet in range(97, 123):
     print("{}".format(chr(alphabet)), end="")
-#The ASCII value of the lowercase alphabet is from 97 to 122.
-#chr() function returns the character that represents the specified unicode.
+# The ASCII value of the lowercase alphabet is from 97 to 122.
+# chr() function returns the character that represents the specified unicode.
