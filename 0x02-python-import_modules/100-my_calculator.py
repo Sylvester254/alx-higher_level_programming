b2a8f444162ad9f2c2a4af3c6418280a310653e3
Sylvester254 <slynganga59@gmail.com>
2022-07-03 20:54:46 +0300

    my calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,8 +7,6 @@ if __name__ == "__main__":
 
     from calculator_1 import add, sub, mul, div
     
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
     count = len(sys.argv) - 1
     if count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
@@ -17,4 +15,6 @@ if __name__ == "__main__":
     if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /\n")
         sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     print(f"{a} {sys.argv[2]} {b} = {ops[sys.argv[2]](a, b)}")
